25bf6012e0419d78d2984723aec033d9f4bc93fb
Kevin Dev <kevin.developer71@gmail.com>
2023-03-25 11:36:04 -0600

    ssg

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,11 +3,11 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import clerkClient from "@clerk/clerk-sdk-node";
-import type { User } from "@clerk/nextjs/dist/api";
 import { TRPCError } from "@trpc/server";
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "@/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 const ratelimit = new Ratelimit({
@@ -15,14 +15,6 @@ const ratelimit = new Ratelimit({
   limiter: Ratelimit.slidingWindow(3, "1 m"),
   analytics: true,
 });
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,24 @@
+import { TRPCError } from "@trpc/server";
+import { clerkClient } from "@clerk/clerk-sdk-node";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { z } from "zod";
+import { filterUserForClient } from "@/server/helpers/filterUserForClient";
+
+export const profileRouter = createTRPCRouter({
+  getUserByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const [user] = await clerkClient.users.getUserList({
+        username: [input.username],
+      });
+
+      if (!user) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "User not found",
+        });
+      }
+
+      return filterUserForClient(user);
+    }),
+});
